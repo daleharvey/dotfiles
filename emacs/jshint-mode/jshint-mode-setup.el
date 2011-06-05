c3c2d052cbc272a948793caae89048d09dae7b8e
Dale Harvey <dale@arandomurl.com>
2011-06-05 20:33:05 +0200

    Removed jshint loader, requires seperate install

--- emacs/jshint-mode/jshint-mode-setup.el
+++ emacs/jshint-mode/jshint-mode-setup.el
@@ -8,9 +8,3 @@
 (defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
   "Prevent annoying \"Active processes exist\" query when you quit Emacs."
   (flet ((process-list ())) ad-do-it))
-
-;; On the fly syntax checking for javascript
-(add-to-list 'load-path "/usr/local/lib/node/.npm/jshint-mode/0.0.2/package")
-(require 'flymake-jshint)
-(add-hook 'javascript-mode-hook
-    (lambda () (flymake-mode t)))
